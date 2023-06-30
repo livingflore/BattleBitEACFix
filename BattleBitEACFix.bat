095b832b48911812c0791143f83a6bd8d1825dc4
livingflore <sergey.tombstone@gmail.com>
2023-07-01 04:52:21 +0500

    optimized + registry entries

--- BattleBitEACFix.bat
+++ BattleBitEACFix.bat
@@ -37,11 +37,14 @@ echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 echo.
-echo Hi. This batch will reinstall EAC and install vcredist if needed. 
+echo Hi. This batch will reinstall EAC and install VCRedist 2015-2022 x86-64. 
 pause
 cls
 
-echo Closing EAC and game...
+SET GamePath=
+FOR /F "tokens=2* skip=2" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 671860" /v "InstallLocation"') do SET GamePath="%%b"
+:: echo %GamePath%
+echo Closing EAC and BattleBit...
 taskkill /f /im BattleBit.exe 2>nul 1>nul
 taskkill /f /im BattleBitEAC.exe 2>nul 1>nul
 taskkill /f /im EasyAntiCheat.exe 2>nul 1>nul
@@ -49,22 +52,29 @@ taskkill /f /im EasyAntiCheat_EOS_Setup.exe 2>nul 1>nul
 taskkill /f /im UnityCrashHandler64.exe 2>nul 1>nul
 
 echo Removing EAC Service...
-FOR /F "tokens=2* skip=2" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 671860" /v "InstallLocation"') do %%b\EasyAntiCheat\EasyAntiCheat_EOS_Setup.exe uninstall 43ed9a4620fa486994c0b368cce73b5d
-FOR /F "tokens=2* skip=2" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 671860" /v "InstallLocation"') do %%b\EasyAntiCheat\EasyAntiCheat_EOS_Setup.exe qa-factory-reset
+%GamePath%\EasyAntiCheat\EasyAntiCheat_EOS_Setup.exe uninstall 43ed9a4620fa486994c0b368cce73b5d
+%GamePath%\EasyAntiCheat\EasyAntiCheat_EOS_Setup.exe qa-factory-reset
 sc delete EasyAntiCheat_EOS 2>nul 1>nul
 sc delete EasyAntiCheat 2>nul 1>nul
 
-echo Removing EAC_EOS folder...
+echo Removing EAC folders...
 rmdir /q /s "C:\Program Files (x86)\EasyAntiCheat_EOS" 2>nul 1>nul
+rmdir /s /q "%temp%/../../Roaming/EasyAntiCheat" 2>nul 1>nul
+rmdir /s /q "%temp%/../../Roaming/EasyAntiCheat" 2>nul 1>nul
+rmdir /s /q "%temp%/../../LocalLow/BattleBitDevTeam" 2>nul 1>nul
+
+Echo Removing EAC registry entries...
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\EasyAntiCheat_EOS" /f 2>nul 1>nul
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\EasyAntiCheat" /f 2>nul 1>nul
 
 echo Installing EAC...
-FOR /F "tokens=2* skip=2" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 671860" /v "InstallLocation"') do %%b\EasyAntiCheat\EasyAntiCheat_EOS_Setup.exe install 43ed9a4620fa486994c0b368cce73b5d
+%GamePath%\EasyAntiCheat\EasyAntiCheat_EOS_Setup.exe install 43ed9a4620fa486994c0b368cce73b5d
 
 echo Switching EAC service mode to automatic...
 sc config EasyAntiCheat_EOS start=Auto 2>nul 1>nul
 
 echo Adding EAC ^& BattleBit Folder to Windows Defender exclusions...
-FOR /F "tokens=2* skip=2" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 671860" /v "InstallLocation"') do powershell -Command Add-MpPreference -ExclusionPath '%%b' > nul
+powershell -Command Add-MpPreference -ExclusionPath %GamePath% > nul
 powershell -Command Add-MpPreference -ExclusionPath 'C:\Program Files (x86)\EasyAntiCheat' > nul
 powershell -Command Add-MpPreference -ExclusionPath 'C:\Program Files (x86)\EasyAntiCheat_EOS' > nul
 
@@ -72,6 +82,8 @@ echo.
 echo EAC reinstall completed.
 echo.
 
+echo Flushing DNS...
+ipconfig /flushdns 2>nul 1>nul
 echo Testing EAC Connection...
 echo download.eac-cdn.com:
 curl download.eac-cdn.com
@@ -90,7 +102,7 @@ if ERRORLEVEL 0 (
    echo Installing VC_redist.x64.exe ^(might take a bit^)...
    %temp%VC_redist.x64.exe /install /quiet /norestart
    if ERRORLEVEL 0 (
-      echo VC_redist.x64.exe installed successful!
+      echo VC_redist.x64.exe installed successfully!
    ) else (
       echo VC_redist.x64.exe install failed with code: %ERRORLEVEL%
    )
@@ -106,7 +118,7 @@ if ERRORLEVEL 0 (
    echo Installing VC_redist.x86.exe ^(might take a bit^)...
    %temp%VC_redist.x86.exe /install /quiet /norestart
    if ERRORLEVEL 0 (
-      echo VC_redist.x86.exe installed successful!
+      echo VC_redist.x86.exe installed successfully!
    ) else (
       echo VC_redist.x86.exe install failed with code: %ERRORLEVEL%
    )
@@ -117,14 +129,20 @@ if ERRORLEVEL 0 (
 
 echo.
 echo Finished!
-
+echo.
+echo =================================================================================
+echo NOTICE: If there's any errors above, screenshot them and send to #anti-cheat-help.
+echo You might also need to select "Install & Repair Easy Anti Cheat" launch option
+echo before actually launching the game.  
+echo =================================================================================
+echo.
 CHOICE /C YN /M "Do you want to reboot your PC (recommended)"
 
 IF %ERRORLEVEL% EQU 1 (
     shutdown /s /f /t 0
 ) ELSE IF %ERRORLEVEL% EQU 2 (
     echo.
-    echo Rebooting your PC is highly recommended, do it before opening your game.
+    echo Rebooting your PC is highly recommended, do it before opening BattleBit.
 )
 
 echo.
